610d892bcbd019ca861b210836bf319245182a6e
TCMAD <tcmada972@gmail.com>
2018-07-10 00:06:03 +0200

    *First*

---
+++ script/animate.js
@@ -0,0 +1,29 @@
+// Menu de gauche
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+// Animation
+AOS.init();
+//Scroll Links
+$(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+	});
+
+//Starter Animations
+$(document).ready(function(){
+  $("main").addClass("magictime foolishIn");
+
+//Scroll Animations
+
+});
